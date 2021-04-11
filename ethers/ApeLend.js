b500ca9aabae2ac7b082a043171ca300a4a4bfd7
RCantu92 <Roberto_Cantu@live.com>
2021-04-11 14:06:45 -0500

    Create ApeLend.js to interact with on the front-end

--- ethers/ApeLend.js
+++ ethers/ApeLend.js
@@ -2,6 +2,12 @@
 import provider from './ethers.js';
 // Import contract ABI
 import BorrowLendProtocol from '../artifacts/contracts/BorrowLendProtocol.sol/BorrowLendProtocol.json';
+// Hardcode apeLend contract address
+// (FIND WAY TO PULL FROM JSON ABI?)
+const apeLendAddress = 0x239eF3B9093fA5fF22a3856aa4bF75EB62072dfA;
 
 // Instance of ApeLend
-const apeLendIntance = new ethers.Contract(/*contract address*/daiAddress, BorrowLendProtocol.abi, provider);
+const apeLendInstance = new ethers.Contract(apeLendAddress, BorrowLendProtocol.abi, provider);
+
+// Export instance of deployed ApeLend
+export default apeLendInstance;
